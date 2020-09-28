edac622ad4a38f2e4a81e817863eac927e1ea4c1
Henrique Fernandes Barreiros <rgmbrazilian@gmail.com>
2020-09-28 15:05:26 -0300

    Create Exercise 5.py

---
+++ Exercises/Exercise 5.py
@@ -0,0 +1,4 @@
+# Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+number = int(input('Olá novamente! Vamos lá! Digite um número inteiro e te diremos seu sucessor e antecessor: '))
+print(f'O número digitado foi {number}, sendo seu sucessor {number + 1} e seu antecessor {number - 1}.')
