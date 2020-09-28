de0d6c9acddf927d6bb31cc357b6bd8f564257d4
Henrique Fernandes Barreiros <rgmbrazilian@gmail.com>
2020-09-28 14:37:10 -0300

    Create Exercise 3.py

---
+++ Exercises/Exercise 3.py
@@ -0,0 +1,9 @@
+# Crie um programa que leia dois número e mostre a soma entre eles.
+
+print('Olá! Vamos fazer uma soma rapidinha? É só seguir as instruções. :)')
+
+number_one = int(input('Favor nos informar o primeiro número: '))
+number_two = int(input('Agora é só nos informar o segundo número, pode deixar o resto com a gente: '))
+soma = (number_one + number_two)
+
+print(f'Muito bem! A soma entre {number_one} e {number_two} é {soma}.')
